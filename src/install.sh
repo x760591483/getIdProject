b22e04cee6ea3b5e30e3418d66c3f93cd56bf415
xxh <760591483@qq.com>
2018-01-06 13:37:50 +0800

    aaa

--- src/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc main.c fileread.c -O getid -L ../lib/ -llogLog -levent -levent_core  -levhtp -levent_openssl -levent_pthreads -ljson -lssl -lcrypto -ldl -lpthread -I ../include/ -I ../include/evhtp/
--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
-./getid -c idget.conf
+./idget -c idget.conf
